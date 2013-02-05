ccdc8ab0f83c6d831a0a5f77611c8f29cc353269
Peter Johnson <pajohnson@email.wm.edu>
2013-02-05 15:02:37 -0500

    Added some additional shells of classes to fill out as I implement my own backend. Currently they are a transparent wrapper around the WXAgg backend.

--- backend_wxagg_erpy.py
+++ backend_wxagg_erpy.py
@@ -1,4 +1,30 @@
+import matplotlib
+from matplotlib.figure import Figure
+from matplotlib.backends import backend_wx
 from matplotlib.backends.backend_wxagg import \
-    backend_version, draw_if_interactive, new_figure_manager, show
+        FigureFrameWxAgg, FigureManagerWx, \
+        backend_version, draw_if_interactive, show
 
 
+def new_figure_manager(num, *args, **kwargs):
+    """
+    Create a new figure manager instance
+    """
+    backend_wx._create_wx_app()
+
+    FigureClass = kwargs.pop('FigureClass', Figure)
+    figure = FigureClass(*args, **kwargs)
+
+    frame = ErpyFigureFrame(num, figure)
+    manager = frame.get_figure_manager()
+    if matplotlib.is_interactive():
+        manager.frame.Show()
+    return manager
+
+
+class ErpyFigureFrame(FigureFrameWxAgg):
+    pass
+
+
+class ErpyFigureManager(FigureManagerWx):
+    pass
