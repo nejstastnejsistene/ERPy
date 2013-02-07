dbafb579ade0c81e1ed7e16993f0095527943dda
Peter Johnson <pajohnson@email.wm.edu>
2013-02-07 14:34:20 -0500

    My backend now successfully redraws while moving via mouse dragging. I need to do some major refactoring regarding the way I'm using and saving variables needed for mpl.draw().

--- backend_wxagg_erpy.py
+++ backend_wxagg_erpy.py
@@ -3,6 +3,7 @@ from matplotlib.figure import Figure
 from matplotlib.backends import backend_wx
 from matplotlib.backends.backend_wxagg import \
         FigureFrameWxAgg, FigureManagerWx, \
+        NavigationToolbar2Wx, \
         backend_version, draw_if_interactive, show
 
 
@@ -13,9 +14,9 @@ def new_figure_manager(num, *args, **kwargs):
     backend_wx._create_wx_app()
 
     FigureClass = kwargs.pop('FigureClass', Figure)
-    figure = FigureClass(*args, **kwargs)
+    fig = FigureClass(*args, **kwargs)
 
-    frame = ErpyFigureFrame(num, figure)
+    frame = ErpyFigureFrame(num, fig)
     manager = frame.get_figure_manager()
     if matplotlib.is_interactive():
         manager.frame.Show()
@@ -23,8 +24,47 @@ def new_figure_manager(num, *args, **kwargs):
 
 
 class ErpyFigureFrame(FigureFrameWxAgg):
-    pass
+
+    def __init__(self, num, fig):
+        FigureFrameWxAgg.__init__(self, num, fig)
+        self.figmgr = ErpyFigureManager(self.canvas, num, self)
+
+    def _get_toolbar(self, statbar):
+        toolbar = ErpyNavigationToolbar(self.canvas)
+        toolbar.set_status_bar(statbar)
+        return toolbar
 
 
 class ErpyFigureManager(FigureManagerWx):
     pass
+    #def __init__(self, canvas, num, frame):
+    #    FigureManagerWx.__init__(self, canvas, num, frame)
+
+
+class ErpyNavigationToolbar(NavigationToolbar2Wx):
+
+    def __init__(self, *args):
+        NavigationToolbar2Wx.__init__(self, *args)
+
+    def dynamic_update(self):
+        #NavigationToolbar2Wx.dynamic_update(self)
+        d = self._idle
+        self._idle = False
+        if d:
+            fig = self.canvas.figure
+            outer_x0, outer_x1 = fig.xlim
+            outer_y0, outer_y1 = fig.ylim
+            axes, = fig.get_axes()
+            inner_x0, inner_x1 = axes.get_xlim()
+            inner_y0, inner_y1 = axes.get_ylim()
+            if inner_x0 < outer_x0 or inner_y0 < outer_y0 or \
+                    inner_x1 > outer_x1 or inner_y1 > outer_y1:
+                print 'reloading buffer'
+                import mpl
+                mpl.draw(fig, axes, fig.data, \
+                        inner_x0, inner_x1, \
+                        inner_y0, inner_y1, \
+                        fig.scale, fig.pixel_density)
+            self.canvas.draw()
+            self._idle = True
+
--- mpl.py
+++ mpl.py
@@ -2,59 +2,74 @@ import struct
 
 import numpy as np
 
-import matplotlib
-matplotlib.use('module://backend_wxagg_erpy')
-from matplotlib import pyplot
 
 filename = '5001.000.b.dat'
 
-# Read the header for information about data.
-with open(filename, 'rb') as f:
-    header = struct.unpack('>20f', f.read(80))
-num_channels = int(header[3])
-sample_rate = header[7]
-total_time = header[9]
-num_samples = int(sample_rate * total_time)
-
-# Load data into numpy memmap.
-shape = num_channels, num_samples
-data = np.memmap(filename, '>f', 'r', 80, shape, 'F')
-
-
-def draw(data, t0, t1, ch0, ch1, scale=5000, pixel_density=1000):
+def draw(fig, axes, data, t0, t1, ch0, ch1, scale=5000, pixel_density=1000):
     dt = t1 - t0
-    dch = ch1 - ch0
+    dch = int(ch1 - ch0)
 
     # Horizontal limits in terms of samples.
-    s0 = max((t0 - dt) * sample_rate, 0)
-    s1 = min((t1 + dt) * sample_rate, num_samples)
+    s0 = max(int((t0 - dt) * fig.sample_rate), 0)
+    s1 = min(int((t1 + dt) * fig.sample_rate), fig.num_samples)
     ds = s1 - s0
     step = ds / pixel_density
 
     # Create the figure and axes.
-    fig = pyplot.figure()
-    axes = pyplot.subplot(111)
+    #fig = pyplot.figure()
+    #axes = pyplot.subplot(111)
+    fig.data = data
+    fig.xlim = t0 - dt, t1 + dt
+    fig.ylim = ch0 - dch, ch1 + dch
+    fig.scale = scale
+    fig.pixel_density = pixel_density
+
+    axes.cla()
 
     # Plot the data.
-    t = (np.arange(s0, s1, dtype=float) / sample_rate)[::step]
-    for i in range(ch0 - 3, ch1 + 3):
-        offset = i + 0.5
+    t = (np.arange(s0, s1, dtype=float) / fig.sample_rate)[::step]
+    off = ch0 % 1
+    for i in range(int(ch0) - dch, int(ch1) + dch):
+        offset = i + 0.5 + off
+        #print offset, i, s0, s1, step
+        #print 'dt', dt, ds/fig.sample_rate
         axes.plot(t, scale*data[i][s0:s1:step]+offset)
-
+        
     # Set our window limits.
     axes.set_xlim(t0, t1)
     axes.set_ylim(ch0, ch1)
     axes.set_xlabel('time (seconds)')
 
-    if False:
+    #if False:
         # Remove margins and axes.
-        fig.subplots_adjust(left=0)
-        fig.subplots_adjust(right=1)
-        fig.subplots_adjust(bottom=0)
-        fig.subplots_adjust(top=1)
-        axes.get_xaxis().set_visible(False)
-        axes.get_yaxis().set_visible(False)
+        #fig.subplots_adjust(left=0)
+        #fig.subplots_adjust(right=1)
+        #fig.subplots_adjust(bottom=0)
+        #fig.subplots_adjust(top=1)
+        #axes.get_xaxis().set_visible(False)
+        #axes.get_yaxis().set_visible(False)
 
+
+if __name__ == '__main__':
+    import matplotlib
+    matplotlib.use('module://backend_wxagg_erpy')
+    from matplotlib import pyplot
+
+    # Read the header for information about data.
+    with open(filename, 'rb') as f:
+        header = struct.unpack('>20f', f.read(80))
+    num_channels = int(header[3])
+    sample_rate = header[7]
+    total_time = header[9]
+    num_samples = int(sample_rate * total_time)
+
+    # Load data into numpy memmap.
+    shape = num_channels, num_samples
+    data = np.memmap(filename, '>f', 'r', 80, shape, 'F')
+
+    fig = pyplot.figure()
+    fig.sample_rate = sample_rate
+    fig.num_samples = num_samples
+    axes = pyplot.subplot(111)
+    draw(fig, axes, data, 5, 8, 5, 8)
     pyplot.show()
-
-draw(data, 5, 8, 5, 8)
